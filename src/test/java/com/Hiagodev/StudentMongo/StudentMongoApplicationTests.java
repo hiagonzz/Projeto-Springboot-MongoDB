33a541917763680b45df47f055b5019f080e65f8
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-22 11:41:57 -0300

    Projeto criado

---
+++ src/test/java/com/Hiagodev/StudentMongo/StudentMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hiagodev.StudentMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

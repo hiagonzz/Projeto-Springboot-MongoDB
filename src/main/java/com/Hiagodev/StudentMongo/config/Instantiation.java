357eddb75212ec8b735f231993c250fdd2055bac
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-23 12:08:17 -0300

    Operação de instanciação de base de dados

---
+++ src/main/java/com/Hiagodev/StudentMongo/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.Hiagodev.StudentMongo.config;
+
+import com.Hiagodev.StudentMongo.domain.User;
+import com.Hiagodev.StudentMongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));5
+
+    }
+}

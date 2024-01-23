7c56082765dfa127e97c82bf3d03627758e833c6
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-01-22 23:45:55 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/Hiagodev/StudentMongo/domain/User.java
+++ src/main/java/com/Hiagodev/StudentMongo/domain/User.java
@@ -1,11 +1,15 @@
 package com.Hiagodev.StudentMongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection = "user")
 public class User implements Serializable {
     public static final long serialVersionUID = 1l;
-
+    @Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/Hiagodev/StudentMongo/resources/UserResource.java
+++ src/main/java/com/Hiagodev/StudentMongo/resources/UserResource.java
@@ -1,26 +1,24 @@
 package com.Hiagodev.StudentMongo.resources;
 
 import com.Hiagodev.StudentMongo.domain.User;
+import com.Hiagodev.StudentMongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity <List<User>> findAll() {
-        User maria = new User("1", "Maria das dores", "Mariaestácomdor@gmail.com");
-        User Alex = new User("2", "alex silva", "Alex@gmail");
-
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, Alex));
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 

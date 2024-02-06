cfef66d16693f3fa1f998b5cd88e31f315dcd6db
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-02-06 08:28:34 -0300

    Referenciando os posts do usuario

--- src/main/java/com/Hiagodev/StudentMongo/config/Instantiation.java
+++ src/main/java/com/Hiagodev/StudentMongo/config/Instantiation.java
@@ -28,14 +28,15 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("18/10/1998"), "Partiu viagem", "Vou viajar para são paulo Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("20/2/2021"), "bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 
 
-
         postRepository.saveAll(Arrays.asList(post1, post2));
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }

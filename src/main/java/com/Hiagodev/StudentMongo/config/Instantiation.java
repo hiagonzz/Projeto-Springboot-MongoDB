d4542b406a47f732b58374462c46bb8755b2e7d7
Hiago nunes pimentel <derundaclash665@gmail.com>
2024-02-06 07:12:50 -0300

    Criando entity post com user aninhado

--- src/main/java/com/Hiagodev/StudentMongo/config/Instantiation.java
+++ src/main/java/com/Hiagodev/StudentMongo/config/Instantiation.java
@@ -1,24 +1,38 @@
 package com.Hiagodev.StudentMongo.config;
 
+import com.Hiagodev.StudentMongo.domain.Post;
 import com.Hiagodev.StudentMongo.domain.User;
+import com.Hiagodev.StudentMongo.repository.PostRepository;
 import com.Hiagodev.StudentMongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
+
 @Configuration
 public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
-
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         userRepository.deleteAll();
+        postRepository.deleteAll();
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
+        Post post1 = new Post(null, sdf.parse("18/10/1998"), "Partiu viagem", "Vou viajar para são paulo Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("20/2/2021"), "bom dia", "Acordei feliz hoje", maria);
+
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/Hiagodev/StudentMongo/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.Hiagodev.StudentMongo.repository;
+
+import com.Hiagodev.StudentMongo.domain.Post;
+import com.Hiagodev.StudentMongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+
+
+}
